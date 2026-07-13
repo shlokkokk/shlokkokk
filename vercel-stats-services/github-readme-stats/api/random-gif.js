40f70040e0b4fa5fa12839ab86e343d93b935047
shlok <shlokshah412@gmail.com>
2026-07-13 21:09:26 +0530

    Update random GIF list

--- vercel-stats-services/github-readme-stats/api/random-gif.js
+++ vercel-stats-services/github-readme-stats/api/random-gif.js
@@ -5,10 +5,19 @@ export default async (req, res) => {
       "https://media1.tenor.com/m/iPRTgyLU3vYAAAAC/jake-perralta-brooklyn99.gif", // Jake Chills
       "https://media1.tenor.com/m/UC_y6M2MG1oAAAAC/brooklyn-nine-nine-b99.gif",      // Captain Holt
       "https://media1.tenor.com/m/EQn9ugqWARsAAAAC/brooklyn-nine.gif",             // Jake Dancing
+      "https://media1.tenor.com/m/fb4HC_jRBQUAAAAC/b99gif-b99.gif",                 // Jake smile
 
       // The Office
       "https://media1.tenor.com/m/Z15l68h1Z5YAAAAC/the-office-handshake.gif",      // Michael Scott Handshake
-      "https://media1.tenor.com/m/Lwz_1N2e400AAAAC/the-office-blinds.gif"          // Jim looking through blinds
+      "https://media1.tenor.com/m/Lwz_1N2e400AAAAC/the-office-blinds.gif",          // Jim looking through blinds
+      "https://media1.tenor.com/m/74qb-GZMXzkAAAAd/the-office-charm.gif",          // Michael Scott charm
+
+      // The Mentalist
+      "https://media1.tenor.com/m/5S8UeFfVRuYAAAAC/the-mentalist-simon-baker.gif", // Patrick Jane
+      "https://media1.tenor.com/m/IbcJZZPrTqEAAAAd/pops-head-up-surprise.gif",      // Patrick Jane pops up
+
+      // Reactions
+      "https://media1.tenor.com/m/nC4k9qCO4s4AAAAC/embarrassed.gif"                 // Jim embarrassed
     ];
 
     const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
